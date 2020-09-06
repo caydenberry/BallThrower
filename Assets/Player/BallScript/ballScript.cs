e7a7619be6d59f0d72e73df7bf19b2a593ac58bc
Cayden <caydendberry@gmail.com>
2020-09-06 10:53:19 -0700

    Reorganized some files and added the goalpost proper
    To do: add ball physics, make player shoot ball, make post interact with ball, fix player's relation to gravity.

--- Assets/Player/BallScript/ballScript.cs
+++ Assets/Player/BallScript/ballScript.cs
@@ -1,13 +1,14 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Goalpost_Script : MonoBehaviour
+public class ballScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //oninteract, check if thing, otherwise nope.
+        
     }
 
     // Update is called once per frame
