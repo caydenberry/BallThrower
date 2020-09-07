362400047741b11cead69551145b617d4436c22d
Cayden <caydendberry@gmail.com>
2020-09-06 18:40:04 -0700

    Fixed some bugs, the ball is instantiated
    The ball doesn't move, but it is instantiated.

---
+++ Assets/Player/Scripts/Shoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] KeyCode shootKey;
+    [SerializeField] GameObject ball;
+    bool isShooting = false;
+    Vector3 shotOrigin;
+    Quaternion shotRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shotOrigin = this.transform.position;
+        shotRotation = this.transform.rotation;
+
+        if (Input.GetKeyDown(shootKey) && isShooting == false)
+        {
+            isShooting = true;
+            Instantiate(ball, shotOrigin, shotRotation);
+            Debug.Log("SHOOT YOUR SHOT");
+        }
+        else
+            isShooting = false;
+    }
+}
