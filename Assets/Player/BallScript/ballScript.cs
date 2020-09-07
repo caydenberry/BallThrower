5a1252bf8c658f42633c71af17e880eae0198c82
Cayden <caydendberry@gmail.com>
2020-09-06 19:01:01 -0700

    I failed you, father
    I tried. I'll work on getting ballscript physics to work tomorrow.

--- Assets/Player/BallScript/ballScript.cs
+++ Assets/Player/BallScript/ballScript.cs
@@ -6,14 +6,28 @@ using UnityEngine;
 public class ballScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    Vector3 frontForce;
+    Rigidbody rb_ball;
+
     void Start()
     {
-        
+        //rb_ball.GetComponent<Rigidbody>();
+
+        //this.AddForce(frontForce, ForceMode.Impulse);
+
+        StartCoroutine(DelayRoutine());
+
+        //Destroy(rb_ball);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator DelayRoutine()
+    {
+        yield return new WaitForSeconds(5);
     }
 }
--- Assets/ScorePost/Scripts/Goalpost_Script.cs
+++ Assets/ScorePost/Scripts/Goalpost_Script.cs
@@ -7,7 +7,7 @@ public class Goalpost_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //oninteract, check if thing, otherwise nope.
+
     }
 
     // Update is called once per frame
